890ba8296f32df3743ba8c44ff95448b9fc068cf
preska <wolfwar@outlook.com>
2023-09-06 13:50:49 +0200

    small fix

--- app/layouts/includes/TopMenu.jsx
+++ app/layouts/includes/TopMenu.jsx
@@ -8,6 +8,7 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 import { useUser } from "@/app/context/user";
 import { useCart } from "@/app/context/cart";
+import ClientOnly from "@/app/components/ClientOnly";
 
 export default function TopMenu() {
   const router = useRouter();
@@ -106,20 +107,22 @@ export default function TopMenu() {
             </li>
 
             {/* // ! cart */}
-            <li className="px-3 hover:underline cursor-pointer">
-              <div onClick={() => router.push("/cart")} className="relative">
-                <AiOutlineShoppingCart size={22} />
-                {cart.cartCount() > 0 ? (
-                  <div className="absolute text-[10px] -top-[2px] -right-[5px] bg-red-500 w-[14px] h-[14px] rounded-full text-white">
-                    <div className=" flex items-center justify-center -mt-[1px]">
-                      {cart.cartCount()}
+            <ClientOnly>
+              <li className="px-3 hover:underline cursor-pointer">
+                <div onClick={() => router.push("/cart")} className="relative">
+                  <AiOutlineShoppingCart size={22} />
+                  {cart.cartCount() > 0 ? (
+                    <div className="absolute text-[10px] -top-[2px] -right-[5px] bg-red-500 w-[14px] h-[14px] rounded-full text-white">
+                      <div className=" flex items-center justify-center -mt-[1px]">
+                        {cart.cartCount()}
+                      </div>
                     </div>
-                  </div>
-                ) : (
-                  <div></div>
-                )}
-              </div>
-            </li>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              </li>
+            </ClientOnly>
           </ul>
         </div>
       </div>
