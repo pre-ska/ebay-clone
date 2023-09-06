5abb965a973b5dcfb1c3dd72e6dbf3840eddd248
preska <wolfwar@outlook.com>
2023-09-06 14:19:28 +0200

    auth page

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
