2fc4c9aa174fba8a3868a0c9365eaaf75194c5f5
preska <wolfwar@outlook.com>
2023-09-06 11:13:30 +0200

    products list main page connected

--- app/page.js
+++ app/page.js
@@ -1,43 +1,44 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import CarouselComp from "./components/CarouselComp";
 import Product from "./components/Product";
 import MainLayout from "./layouts/MainLayout";
-
-const products = [
-  {
-    id: 1,
-    title: "Brown letter bag",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quisquam.",
-    url: "https://picsum.photos/id/7",
-    price: 2500,
-  },
-  {
-    id: 2,
-    title: "School books",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quisquam.",
-    url: "https://picsum.photos/id/20",
-
-    price: 1990,
-  },
-];
+import useIsLoading from "@/app/hooks/useIsLoading";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    useIsLoading(true);
+
+    const response = await fetch("/api/products");
+    const prods = await response.json();
+
+    setProducts([]);
+    setProducts(prods);
+    useIsLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <MainLayout>
-      <CarouselComp />
+    <>
+      <MainLayout>
+        <CarouselComp />
 
-      <div className="max-w-[1200px] mx-auto">
-        <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
+        <div className="max-w-[1200px] mx-auto">
+          <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
 
-        <div className="grid grid-cols-5 gap-4">
-          {products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
+          <div className="grid grid-cols-5 gap-4">
+            {products.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
         </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </>
   );
 }
